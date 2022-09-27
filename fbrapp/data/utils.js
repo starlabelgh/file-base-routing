b1f9ffd5a623f4b541bb13ba79c1a175377f13d2
starlabel <hello@starlabel.co>
2022-09-27 20:23:00 +0000

    added new books data and call from the data api to display on the books page

---
+++ fbrapp/data/utils.js
@@ -0,0 +1,26 @@
+export const books = [{
+    id: "1",
+    name: "Mindset",
+    description: "This is my First Book",
+},
+{
+    id: "2",
+    name: "The Octopus",
+    description: "This is my First Book",
+},
+{
+    id: "3",
+    name: "Modern Entrepreneur",
+    description: "This is my First Book",
+},
+{
+    id: "4",
+    name: "Smart Money",
+    description: "This is my First Book",
+},
+{
+    id: "5",
+    name: "Think and Grow Rich",
+    description: "This is my First Book",
+},
+];
--- fbrapp/pages/books/index.js
+++ fbrapp/pages/books/index.js
@@ -1,5 +1,33 @@
+import { books } from '../../data/utils'
+import React from 'react'
+import Link from 'next/link'
 function BookHome() {
-    return <h1>Books Homepage</h1>
+    return <div>
+        {books.map((book, index) => (
+            <div style={{
+                width: 300,
+                background: "whitesmoke",
+                margin: 'auto',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}key={index}
+            >
+                <h2>{book.name}</h2>
+                <p>{book.description}</p>
+                <article 
+                style={{
+                    border: '1px solid black', 
+                    padding: 12,
+                    background: "#ccc"
+                    }}
+                    >
+                        <Link href={`/books/${book.id}`}>Detail</Link>
+                        </article>
+            </div>
+        ))}
+    </div>
 }
 
-export default BookHome
+export default BookHome;
